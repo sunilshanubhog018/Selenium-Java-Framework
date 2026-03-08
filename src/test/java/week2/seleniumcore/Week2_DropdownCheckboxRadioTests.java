3cc0a4518c7be4f17eba2fef039eb6421f039d7c
sunilshanubhog018 <sunilshanubhog018@gmail.com>
2026-03-08 14:24:36 +0530

    All test cases running under CI/CD

--- src/test/java/week2/seleniumcore/Week2_DropdownCheckboxRadioTests.java
+++ src/test/java/week2/seleniumcore/Week2_DropdownCheckboxRadioTests.java
@@ -6,11 +6,9 @@ import java.time.Duration;
 import java.util.List;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 import org.testng.Assert;
 import org.openqa.selenium.JavascriptExecutor;
