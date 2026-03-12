ef605d3b82d46e92b9ff6edc3d3d23c92e88ba83
sunilshanubhog018 <sunilshanubhog018@gmail.com>
2026-03-12 09:57:27 +0530

    All test cases running under CI/CD

--- src/test/java/LocatorsPractice/LocatorTests.java
+++ src/test/java/LocatorsPractice/LocatorTests.java
@@ -25,7 +25,11 @@ public void testValidLogin() {
     driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
     driver.findElement(By.cssSelector("button[type='submit']")).click();
     
-    String message = driver.findElement(By.id("flash")).getText();
+    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    WebElement flashMessage = wait.until(
+        ExpectedConditions.visibilityOfElementLocated(By.id("flash"))
+    );
+    String message = flashMessage.getText();
     Assert.assertTrue(message.contains("You logged into a secure area!"));
 }
 }//input[@id='username']
